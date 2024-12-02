97329574661c69810477d8177d3e6924ce22df8a
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-12-02 18:29:26 -0500

    04 done

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,21 @@
 function changeContext(func, obj) {
-  // Your code here
+  let newFunc = func.bind(obj);
+
+  return newFunc()
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
